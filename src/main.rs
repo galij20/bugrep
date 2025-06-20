a8e68936919bb9cbca873bc25e2c57e54af82189
Galij Sunuwar <196127646+galij20@users.noreply.github.com>
2025-06-20 23:41:00 +0545

    Add lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use bugrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,36 +16,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     
-    if let Err(e) = run(config) {
+    if let Err(e) = bugrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
- 
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text: \n{contents}");
-    
-    Ok(())
-}
