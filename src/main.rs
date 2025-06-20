dacddfacaa2c8564fd2fc92bfcee0414250947a7
Galij Sunuwar <196127646+galij20@users.noreply.github.com>
2025-06-20 23:35:29 +0545

    Add proper error handling and modularity

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@
 #![allow(unused_imports)]
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    }); 
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -22,9 +27,25 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+ 
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
     
-    Config { query, file_path }
+        Ok(Config { query, file_path })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text: \n{contents}");
+    
+    Ok(())
 }
